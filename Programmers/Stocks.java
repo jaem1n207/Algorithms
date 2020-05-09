21801fb813f8a04636d7e258fbd13f2c760b4e13
jaem1n207 <jaem1n207@naver.com>
2020-05-10 00:37:35 +0900

    [ADD] Stocks

---
+++ Programmers/Stocks.java
@@ -0,0 +1,15 @@
+
+public class Stocks {
+	public static void main(String[] args) {
+		Stocks main = new Stocks();
+		int[] prices = {1, 2, 3, 2, 3};
+		
+		System.out.println(main.solution(prices));
+	}
+	
+	public int solution(int[] prices) {
+		int answer = 0;
+		
+		return answer;
+	}
+}
