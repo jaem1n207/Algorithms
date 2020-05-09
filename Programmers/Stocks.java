39ee598c4d35d2a2dc966e9f7e0c461b96453cfc
jaem1n207 <jaem1n207@naver.com>
2020-05-10 01:00:14 +0900

    [UPDATE] Stocks

--- Programmers/Stocks.java
+++ Programmers/Stocks.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Stocks {
 	public static void main(String[] args) {
@@ -7,8 +8,20 @@ public class Stocks {
 		System.out.println(main.solution(prices));
 	}
 	
-	public int solution(int[] prices) {
-		int answer = 0;
+	public int[] solution(int[] prices) {
+		int[] answer = new int[prices.length];
+		for(int i = 0; i < prices.length; i++) {
+			for(int j = i + 1; j < prices.length; j++) {
+				if(prices[i] > prices[j]) {
+					answer[i] = j - i;
+					break;
+				} 
+				else
+					answer[i] = j - i;
+			}
+		}
+		
+		System.out.println(Arrays.toString(answer));
 		
 		return answer;
 	}
